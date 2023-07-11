b4a7ba4b07dd6575f488cdf411039f49543b377c
Pritika Shrestha <pritikastha1@gmail.com>
2023-07-11 22:07:34 +0545

    inheritance

---
+++ Inheritance/multipleconstbaseandsinglederived.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class base
+{
+    public:
+    base()
+    {
+        cout<<"default constructor from base class"<<endl;
+    }
+    base(int data)
+    {
+        cout<<"parameterized constructor from base class"<<endl;
+    }
+};
+class derived:public base
+{
+    public:
+    derived(int data)
+    {
+        cout<<"paramterizrd constructor from derived class";
+    }
+};
+int main()
+{
+    derived d(5);
+}
---
+++ Inheritance/passingparametertomulipleconstsructor.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+class base_one
+{
+    public:
+    base_one()
+    {
+        cout<<"no parameter constructor from base_one class";
+    }
+    base_one(int x)
+    {
+        cout<<"\nparameterized constructor from base_one class"<<endl;
+    }
+};
+class base_two
+{
+    public:
+    base_two()
+    {
+        cout<<"\nno parameter constructor from base_two class";
+    }
+    base_two(int y)
+    {
+        cout<<"\nparameter constructor from base_two class"<<endl;
+}
+};
+class derived:public base_one,public base_two{
+    public:
+    derived()
+    {
+        cout<<"\nno parameterized contructor from derived class"<<endl;
+    }
+    derived(int x,int y,int z):base_one(x),base_two(y)
+    {
+        cout<<"\nparameterized constructor from derived class"<<endl;
+    }
+};
+int main()
+{
+    derived d1;
+    derived d(2,3,4);
+}
